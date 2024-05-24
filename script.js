675add19b4b2fc8992aa95194040043e5ad0231e
Nguyễn Hữu Bách <114559382+KimiZK-Dev@users.noreply.github.com>
2024-05-24 19:48:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ const digg_count = document.querySelector(".digg_count");
 const download_count = document.querySelector(".download_count");
 const play = document.querySelector(".play_count");
 const duration = document.querySelector(".duration");
-const preview = document.querySelector(".preview");
+const preview = document.querySelector(".preVid");
+const pageDown = document.querySelector(".pageDown");
 const pic = document.querySelector(".pic");
-const pics = document.querySelector(".pics");
-const imgPreview = document.querySelector(".preview img");
+const downPic = document.querySelector(".downPic");
+const prePics = document.querySelector(".prePics");
+const imgPreview = document.querySelector(".preVid img");
 const downVid = document.querySelector(".downVid");
 const downAudio = document.querySelector(".downAudio");
 
@@ -20,7 +22,6 @@ function getData(api, url) {
     const data = res.data.data;
 
     if (res.data.code == 0) {
-      console.log(data);
       const durationInMinutes = Math.floor(data.duration / 60);
       const remainingSeconds = data.duration % 60;
       const formattedDuration = `${durationInMinutes} phút ${remainingSeconds} giây`;
@@ -30,7 +31,6 @@ function getData(api, url) {
         (data.size
           ? (data.size / 1000000).toFixed(3) + "MB"
           : "Mỗi ảnh kích thước khác nhau!");
-
       ID.innerHTML = "UserID: " + data.author.unique_id;
       nickname.innerHTML = "Tên tài khoản: " + data.author.nickname;
       title.innerHTML = "Tiêu đề bài viết: " + data.title;
@@ -40,30 +40,76 @@ function getData(api, url) {
       duration.innerHTML = "Thời gian bài viết: " + formattedDuration;
       play.innerHTML = "Số lượt xem: " + data.play_count;
       imgPreview.src = data.ai_dynamic_cover;
-      test(downVid, data.play);
-      test(downAudio, data.music);
+      getDataForVid(downVid, data.play);
+      getDataForVid(downAudio, data.music);
 
-      if (data.images) {
-        if (preview) {
-          preview.remove();
+      async function processImages(data) {
+        if (data.images) {
+          downVid.remove();
+          if (preview) {
+            preview.remove();
+          } else {
+            console.log("ok");
+            pic.remove();
+          }
+
+          const zip = new JSZip();
+          const folder = zip.folder("images");
+
+          for (let i = 0; i < data.images.length; i++) {
+            const e = document.createElement("div");
+            const eC = document.createElement("img");
+            const downPic = document.createElement("a");
+
+            e.className = "pic";
+            eC.src = data.images[i];
+
+            const { url, file } = await getDataForPic(data.images[i]);
+            if (url && file) {
+              downPic.href = url;
+              downPic.download = "Down";
+              downPic.className = "downPic";
+              downPic.textContent = "Tải về";
+
+              // Add file to the ZIP archive
+              folder.file(`image${i + 1}.${file.type.split("/")[1]}`, file);
+
+              prePics.appendChild(e);
+              e.appendChild(eC);
+              e.appendChild(downPic);
+            } else {
+              console.error(
+                "Failed to create download URL for image",
+                data.images[i]
+              );
+            }
+          }
+
+          const zipButton = document.createElement("a");
+          const icon = document.createElement("i");
+
+          zipButton.className = "downPics";
+          icon.className = "fa-duotone fa-download";
+
+          zipButton.appendChild(icon);
+          zipButton.appendChild(document.createTextNode("ALBUM"));
+
+          zipButton.addEventListener("click", async () => {
+            const content = await zip.generateAsync({ type: "blob" });
+            const zipUrl = URL.createObjectURL(content);
+            const downPics = document.createElement("a");
+            downPics.href = zipUrl;
+            downPics.download = "images.zip";
+            downPics.click();
+            URL.revokeObjectURL(zipUrl);
+          });
+
+          pageDown.appendChild(zipButton);
         } else {
-          console.log("ok");
-          pic.remove();
+          document.getElementById("t").innerHTML = "LINK NAY DELL CO ANH";
         }
-        const picsElement = document.querySelector(".pics");
-        for (let i = 0; i < data.images.length; i++) {
-          const picsCLass = document.createElement("div");
-          picsCLass.classList.add("pic");
-          const pic = document.createElement("img");
-          pic.src = data.images[i];
-          pic.alt = `Preview ${i}`;
-          picsCLass.appendChild(pic);
-          picsElement.appendChild(picsCLass);
-        }
-      } else {
-        pic.remove();
-        document.getElementById("t").innerHTML = "LINK NAY DELL CO ANH";
       }
+      processImages(data);
     } else {
       console.log(res.data.code);
       alert(
@@ -73,14 +119,29 @@ function getData(api, url) {
   });
 }
 
-function test(element, data) {
+function getDataForVid(element, data) {
   fetch(data)
     .then((res) => res.blob())
     .then((blob) => {
       const file = new File([blob], "image", { type: blob.type });
       const url = URL.createObjectURL(file);
+
       element.href = url;
+      console.log(url);
     });
+}
+
+async function getDataForPic(data) {
+  try {
+    const res = await fetch(data);
+    const blob = await res.blob();
+    const file = new File([blob], "image", { type: blob.type });
+    const url = URL.createObjectURL(file);
+    return { url, file };
+  } catch (error) {
+    console.error("Error fetching image:", error);
+    return null;
+  }
 }
 
 function clearSearch() {
