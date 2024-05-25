bcb7f4d9cf5ca5adebfda5bb08ca20f02b16c4b0
Nguyễn Hữu Bách <114559382+KimiZK-Dev@users.noreply.github.com>
2024-05-25 11:00:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,122 +1,211 @@
 const apiLink = "https://www.tikwm.com/api/?url=";
-const size = document.querySelector(".size");
-const ID = document.querySelector(".authorID");
-const nickname = document.querySelector(".authorName");
-const title = document.querySelector(".title");
-const comment_count = document.querySelector(".comment_count");
-const digg_count = document.querySelector(".digg_count");
-const download_count = document.querySelector(".download_count");
-const play = document.querySelector(".play_count");
-const duration = document.querySelector(".duration");
-const preview = document.querySelector(".preVid");
-const pageDown = document.querySelector(".pageDown");
-const pic = document.querySelector(".pic");
-const downPic = document.querySelector(".downPic");
-const prePics = document.querySelector(".prePics");
-const imgPreview = document.querySelector(".preVid img");
-const downVid = document.querySelector(".downVid");
-const downAudio = document.querySelector(".downAudio");
+const e = {
+  des: document.querySelector(".des"),
+  info: document.querySelector(".info"),
+  size: document.querySelector(".size"),
+  ID: document.querySelector(".authorID"),
+  nickname: document.querySelector(".authorName"),
+  title: document.querySelector(".title"),
+  comment_count: document.querySelector(".comment_count"),
+  digg_count: document.querySelector(".digg_count"),
+  download_count: document.querySelector(".download_count"),
+  play: document.querySelector(".play_count"),
+  duration: document.querySelector(".duration"),
+  preview: document.querySelector(".preVid"),
+  preVid: document.querySelector(".preVid img"),
+  prePics: document.querySelector(".prePics"),
+  pic: document.querySelectorAll(".pic"),
+  pageDown: document.querySelector(".pageDown"),
+  downPic: document.querySelector(".downPic"),
+  imgPreview: document.querySelector(".preVid img"),
+  downVid: document.querySelector(".downVid"),
+  downAudio: document.querySelector(".downAudio"),
+  container: document.querySelector("#s2 .container"),
+};
 
-function getData(api, url) {
-  axios.get(`${api}${url}`).then((res) => {
-    const data = res.data.data;
-
-    if (res.data.code == 0) {
-      const durationInMinutes = Math.floor(data.duration / 60);
-      const remainingSeconds = data.duration % 60;
-      const formattedDuration = `${durationInMinutes} phút ${remainingSeconds} giây`;
-
-      size.innerHTML =
-        "Dung lượng: " +
-        (data.size
-          ? (data.size / 1000000).toFixed(3) + "MB"
-          : "Mỗi ảnh kích thước khác nhau!");
-      ID.innerHTML = "UserID: " + data.author.unique_id;
-      nickname.innerHTML = "Tên tài khoản: " + data.author.nickname;
-      title.innerHTML = "Tiêu đề bài viết: " + data.title;
-      comment_count.innerHTML = "Số bình luận: " + data.comment_count;
-      digg_count.innerHTML = "Số tim: " + data.digg_count;
-      download_count.innerHTML = "Số lượt tải xuống: " + data.download_count;
-      duration.innerHTML = "Thời gian bài viết: " + formattedDuration;
-      play.innerHTML = "Số lượt xem: " + data.play_count;
-      imgPreview.src = data.ai_dynamic_cover;
-      getDataForVid(downVid, data.play);
-      getDataForVid(downAudio, data.music);
-
-      async function processImages(data) {
-        if (data.images) {
-          downVid.remove();
-          if (preview) {
-            preview.remove();
-          } else {
-            console.log("ok");
-            pic.remove();
-          }
-
-          const zip = new JSZip();
-          const folder = zip.folder("images");
-
-          for (let i = 0; i < data.images.length; i++) {
-            const e = document.createElement("div");
-            const eC = document.createElement("img");
-            const downPic = document.createElement("a");
-
-            e.className = "pic";
-            eC.src = data.images[i];
-
-            const { url, file } = await getDataForPic(data.images[i]);
-            if (url && file) {
-              downPic.href = url;
-              downPic.download = "Down";
-              downPic.className = "downPic";
-              downPic.textContent = "Tải về";
-
-              // Add file to the ZIP archive
-              folder.file(`image${i + 1}.${file.type.split("/")[1]}`, file);
-
-              prePics.appendChild(e);
-              e.appendChild(eC);
-              e.appendChild(downPic);
-            } else {
-              console.error(
-                "Failed to create download URL for image",
-                data.images[i]
-              );
-            }
-          }
-
-          const zipButton = document.createElement("a");
-          const icon = document.createElement("i");
-
-          zipButton.className = "downPics";
-          icon.className = "fa-duotone fa-download";
-
-          zipButton.appendChild(icon);
-          zipButton.appendChild(document.createTextNode("ALBUM"));
-
-          zipButton.addEventListener("click", async () => {
-            const content = await zip.generateAsync({ type: "blob" });
-            const zipUrl = URL.createObjectURL(content);
-            const downPics = document.createElement("a");
-            downPics.href = zipUrl;
-            downPics.download = "images.zip";
-            downPics.click();
-            URL.revokeObjectURL(zipUrl);
-          });
-
-          pageDown.appendChild(zipButton);
-        } else {
-          document.getElementById("t").innerHTML = "LINK NAY DELL CO ANH";
-        }
-      }
-      processImages(data);
+async function fetchData(api, url) {
+  try {
+    const res = await axios.get(`${api}${url}`);
+    if (res.data.code === 0) {
+      e.des.remove();
+      return res.data.data;
     } else {
-      console.log(res.data.code);
+      console.error(`Error code: ${res.data.code}`);
       alert(
         "Vui lòng nhập link cần lấy dữ liệu!!\nNếu không được bạn vui lòng kiểm tra lại link <3"
       );
+      return null;
     }
-  });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    alert("Có lỗi xảy ra khi lấy dữ liệu!");
+    return null;
+  }
+}
+
+function formatDuration(duration) {
+  const minutes = Math.floor(duration / 60);
+  const seconds = duration % 60;
+  return `${minutes} phút ${seconds} giây`;
+}
+
+async function getData(api, url) {
+  const data = await fetchData(api, url);
+  console.log(data);
+  if (!data) return;
+
+  const fields = [
+    {
+      icon: "fa-duotone fa-id-badge",
+      className: "authorID",
+      text: `UserID: ${data.author.unique_id}`,
+    },
+    {
+      icon: "fa-duotone fa-user",
+      className: "authorName",
+      text: `Tên tài khoản: ${data.author.nickname}`,
+    },
+    {
+      icon: "fa-duotone fa-subtitles",
+      className: "title",
+      text: `Tiêu đề bài viết: ${data.title}`,
+    },
+    {
+      icon: "fa-duotone fa-comments",
+      className: "comment_count",
+      text: `Số bình luận: ${data.comment_count}`,
+    },
+    {
+      icon: "fa-duotone fa-circle-heart",
+      className: "digg_count",
+      text: `Số tim: ${data.digg_count}`,
+    },
+    {
+      icon: "fa-duotone fa-circle-down",
+      className: "download_count",
+      text: `Số lượt tải xuống: ${data.download_count}`,
+    },
+    {
+      icon: "fa-duotone fa-timer",
+      className: "duration",
+      text: `Thời gian bài viết: ${formatDuration(data.duration)}`,
+    },
+    {
+      icon: "fa-duotone fa-circle-play",
+      className: "play_count",
+      text: `Số lượt xem: ${data.play_count}`,
+    },
+    {
+      icon: "fa-duotone fa-file",
+      className: "size",
+      text: `Dung lượng: ${
+        data.size
+          ? (data.size / 1000000).toFixed(3) + "MB"
+          : "Mỗi ảnh kích thước khác nhau!"
+      }`,
+    },
+  ];
+  if (!e.info) {
+    const infoDiv = document.createElement("div");
+    infoDiv.className = "info";
+
+    fields.forEach((field) => {
+      const div = document.createElement("div");
+      const icon = document.createElement("i");
+      const span = document.createElement("span");
+
+      div.className = "i1";
+      icon.className = field.icon;
+      span.className = field.className;
+      span.innerHTML = field.text;
+
+      div.appendChild(icon);
+      div.appendChild(span);
+      infoDiv.appendChild(div);
+    });
+
+    e.container.appendChild(infoDiv);
+    e.info = true;
+  } else {
+    fields.forEach((field) => {
+      document.querySelector("." + field.className).innerHTML = field.text;
+    });
+  }
+
+  if (data.images) {
+    async function processImages() {
+      if (e.pic) {
+        const ePics = e.prePics.querySelectorAll(".pic");
+        // e.downPic.remove();
+        ePics.forEach((picElement) => {
+          picElement.remove();
+        });
+        console.log("ok");
+      }
+      if (data.images) {
+        e.preview.remove();
+        e.downVid.remove();
+        getDataForVid(e.downAudio, data.play);
+        const zip = new JSZip();
+        const folder = zip.folder("images");
+
+        data.images.forEach(async (imageUrl, i) => {
+          const imageDiv = document.createElement("div");
+          const img = document.createElement("img");
+          const downLink = document.createElement("a");
+
+          imageDiv.className = "pic";
+          img.src = imageUrl;
+
+          const { url, file } = await getDataForPic(imageUrl);
+
+          if (url && file) {
+            downLink.href = url;
+            downLink.download = `Ảnh ${i + 1}`;
+            downLink.className = "downPic";
+            downLink.textContent = "Tải về";
+
+            folder.file(`image${i + 1}.${file.type.split("/")[1]}`, file);
+
+            imageDiv.appendChild(img);
+            imageDiv.appendChild(downLink);
+            e.prePics.appendChild(imageDiv);
+          } else {
+            console.error("Failed to create download URL for image", imageUrl);
+          }
+        });
+
+        const zipButton = document.createElement("a");
+        const icon = document.createElement("i");
+
+        zipButton.className = "downPics";
+        icon.className = "fa-duotone fa-download";
+
+        zipButton.appendChild(icon);
+        zipButton.appendChild(document.createTextNode("ALBUM"));
+
+        zipButton.addEventListener("click", async () => {
+          const content = await zip.generateAsync({ type: "blob" });
+          const zipUrl = URL.createObjectURL(content);
+          const downLink = document.createElement("a");
+          downLink.href = zipUrl;
+          downLink.download = "images.zip";
+          downLink.click();
+          URL.revokeObjectURL(zipUrl);
+        });
+
+        e.pageDown.insertBefore(zipButton, e.pageDown.firstChild);
+      } else {
+        document.getElementById("t").innerHTML = "LINK NÀY KHÔNG CÓ ẢNH";
+      }
+    }
+
+    processImages();
+  } else {
+    e.preVid.src = data.ai_dynamic_cover;
+    getDataForVid(e.downVid, data.play);
+    getDataForVid(e.downAudio, data.play);
+  }
 }
 
 function getDataForVid(element, data) {
@@ -125,9 +214,7 @@ function getDataForVid(element, data) {
     .then((blob) => {
       const file = new File([blob], "image", { type: blob.type });
       const url = URL.createObjectURL(file);
-
       element.href = url;
-      console.log(url);
     });
 }
 
